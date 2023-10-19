247565a908aefc667440f77d6940f65ae8211b26
Desmond Tong <tysdesmond2@gmail.com>
2023-10-20 01:09:10 +0800

    Setup routes

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,15 +1,19 @@
-import { useState } from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes } from "react-router-dom";
+
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import AdminOnly from "./pages/AdminOnly";
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/register" element={<Register />}></Route>
+        <Route path="/adminonly" element={<AdminOnly />}></Route>
       </Routes>
-
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
