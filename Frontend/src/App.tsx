ce5a16a04f0391f7ccdbc39d52abebf859a7aca5
Desmond Tong <tysdesmond2@gmail.com>
2023-10-20 01:53:25 +0800

    Add homepage

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import AdminOnly from "./pages/AdminOnly";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
@@ -10,6 +11,8 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />}></Route>
         <Route path="/register" element={<Register />}></Route>
+        <Route path="/homepage" element={<HomePage />}></Route>
+        
         <Route path="/adminonly" element={<AdminOnly />}></Route>
       </Routes>
     </div>
