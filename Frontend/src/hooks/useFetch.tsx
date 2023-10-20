458de5d47aad1eb0e4cc04dd5920220fd0ce427d
Desmond Tong <tysdesmond2@gmail.com>
2023-10-20 11:41:12 +0800

    Comment out Authorization in useFetch custom hook

--- Frontend/src/hooks/useFetch.tsx
+++ Frontend/src/hooks/useFetch.tsx
@@ -8,7 +8,7 @@ const useFetch = () => {
       method,
       headers: {
         "Content-Type": "application/json",
-        Authorization: "Bearer " + token,
+        // Authorization: "Bearer " + token,
       },
       body: JSON.stringify(body),
     });
